106d9d41b5dfb0b5da76fa925a627d45c2a0d5c3
xujinghua <jinghua.xu@student.uni-tuebingen.de>
2019-11-29 16:24:21 +0100

    ex01 test failed, min_edit_dist functions correctly

--- spell_checker/spell_checker.py
+++ spell_checker/spell_checker.py
@@ -2,7 +2,12 @@
    
     Problem 1: Spell checker.
 
-    <Please insert your data and the honor code here.>
+    Course:      Data Structures and Algorithms for CL III - WS1920
+    Assignment:  lab 2, exercise 1
+    Author:      Jinghua Xu
+    Description: A spell-checker class that stores a lexicon of words lexicon, in a Python set, and implements a method, check(word), which performs a spell check on the string word with respect to the words in lexicon.
+ 
+    Honor Code:  I pledge that this program represents my own work.
 """
 
 class SpellChecker:
@@ -16,9 +21,7 @@ class SpellChecker:
         lexicon : iterable
             An iterable collection of over the words that should be used as a dictionary for this spell checker.
         """
-        self._lexicon = None
-
-        # FIXME your code goes here
+        self._lexicon = set(lexicon)
 
     @property
     def lexicon(self):
@@ -39,6 +42,59 @@ class SpellChecker:
         """
         spellings = []
 
-        # FIXME your code goes here
+        if word in self._lexicon:
+            spellings.append(word)
+
+        else:
+            for item in self._lexicon:
+                dist = self.min_edit_distance(word, item)
+                if dist == 1 or dist == 2:
+                    spellings.append(item)
 
         return spellings
+
+    def min_edit_distance(self, source, target):
+
+        n = len(source)
+        m = len(target)
+        # create a distance matrix distance[n+1, m+1], temporarily silled in with 'None'
+        distance = []
+        for i in range(n+1):
+            distance.append([])
+            for j in range(m+1):
+                distance[i].append(0)
+            
+        # initialization: the zeroth row and col is the distance from the empty string
+        distance[0][0] = 0
+
+        # initiallize first col
+        for i in range(1, n+1):
+            distance[i][0] = distance[i-1][0] + 1
+        # initialize first row
+        for j in range(1, m+1):
+            distance[0][j] = distance[0][j-1] +1
+
+        #recurrence relation
+        for i in range(1,n+1):
+            for j in range(1, m+1):
+                # cell to the left in table
+                left = distance[i-1][j] + 1
+                # cell above in table
+                up = distance[i][j-1] + 1
+                diagonal = distance[i-1][j-1]
+                if source[i-1] != target[j-1]:
+                    diagonal += 2
+                distance[i][j] = min(left, up, diagonal)
+
+        return distance[n][m]
+
+
+#min_edit_diatnce() works
+def main():
+        tmp = SpellChecker({1})
+        # 50 : no correct spelling identified for misspelling  carusell . The correct spelling was  carousel .
+        print(tmp.min_edit_distance('carusell','carousel'))
+
+if __name__ == "__main__":
+        main()
+    
