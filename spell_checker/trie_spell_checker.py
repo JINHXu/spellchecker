66844068811bb1128f34c8b065369556e8f648ea
xujinghua <jinghua.xu@student.uni-tuebingen.de>
2019-12-05 10:54:13 +0100

    ex03 start over lol

--- spell_checker/trie_spell_checker.py
+++ spell_checker/trie_spell_checker.py
@@ -2,15 +2,17 @@
    
     Problem 3: Spell checker using a trie.
 
-    <Please insert your data and the honor code here.>
+    Course:      Data Structures and Algorithms for CL III - WS1920
+    Assignment:  lab 2, exercise 3
+    Author:      Jinghua Xu
+    Description: A spell checker implemented with trie, i.e. lexicon will be stored in a trie instead of a set.
+    Honor Code:  I pledge that this program represents my own work.
 """
 
 class TrieSpellChecker:
 
     def __init__(self, lexicon):
-        self._lexicon = None
-
-        # FIXME your code goes here
+        self._lexicon = StandardTrie(lexicon)
 
 
     @property
