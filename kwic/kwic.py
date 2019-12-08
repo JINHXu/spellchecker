24ad3dff307e01bd60a193d7a06ec80780669b30
xujinghua <jinghua.xu@student.uni-tuebingen.de>
2019-12-08 16:27:28 +0100

    works(output to be adjusted)

--- kwic/kwic.py
+++ kwic/kwic.py
@@ -11,6 +11,8 @@
     Honor Code:  I pledge that this program represents my own work.
 """
 
+from kwic.word_matching_trie import WordMatchingTrie
+
 import argparse
 
 if __name__ == "__main__":
@@ -18,7 +20,53 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--file", type=argparse.FileType(mode="r", encoding="utf-8"), default="kwic/data/2701-0.txt")
     parser.add_argument("-w", "--window-size", type=int, default=50)
     parser.add_argument("-m", "--max-count", type=int, default=10)
+    # key word to search 
+    parser.add_argument("-k", "--keyword", type=str, default='')
     args = parser.parse_args()
+
+    text = ''
+    # text processing
+    wrapper = args.file
+    lines = wrapper.readlines()
+    for line in lines:
+        # remove any newlines from the text
+        text += line.strip('\n')
     
-    # FIXME your code goes here
-    
+    # trie to store text
+    trie = WordMatchingTrie(text)
+
+    # The program should keep asking for a new keyword until the user terminates the loop by presses enter without inputting any characters.
+    keyword = args.keyword
+
+    while True:
+        values = trie.find(keyword)  
+        # query the text for the keyword
+        if values:
+            # it should print the args.max_count contexts for the word
+            values = values[:args.max_count]
+            for idx in values:
+                # args.w chars(including spaces) to the left of keyword
+                context_to_left = text[idx-args.window_size: idx]
+                # args.w chars(including spaces) to the right of keyword
+                context_to_right = text[idx+1: idx+args.window_size]
+                # In cases where the contexts are smaller than window_sz
+                if len(context_to_left) < args.window_size:
+                    #justified output
+                    num_spaces = args.max_count - len(context_to_left)
+                    print(' '*num_spaces + context_to_left + ' '*4 + keyword + ' '*4 + context_to_right)
+                else:
+                    print(context_to_left + ' '*4 + keyword + ' '*4 + context_to_right)
+
+        else:
+            print('Keyword not found in text.')
+        
+        nextcommand = input("Enter new keyword to search or press Enter to quit:")
+
+        if nextcommand == '':
+            break
+        else:
+            keyword = nextcommand
+
+        
+            
+        
